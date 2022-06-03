2288072dbf605ce5473dcc4e80fe32f83dd4c305
azurite17@gmail.com <azurite17@gmail.com>
2022-06-03 11:36:05 +0900

    2022/06/03

---
+++ Book/Ch02/P83.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Ch02
+{
+    internal class P83
+    {
+        static void Main1(string[] args)
+        {
+            uint unsignedInt = 414783647;
+            ulong unsignedLong = 11223372036854775808;
+
+            Console.WriteLine(unsignedInt);
+            Console.WriteLine(unsignedLong);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+        }
+    }
+}
