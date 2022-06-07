f37d953b2df2e0e869ef6035341cbffaae2162a5
azurite17@gmail.com <azurite17@gmail.com>
2022-06-07 11:15:35 +0900

    2022/06/07

--- Ch03/3_For.cs
+++ Ch03/3_For.cs
@@ -6,7 +6,74 @@ using System.Threading.Tasks;
 
 namespace Ch03
 {
+    // 157p
     internal class _3_For
     {
+        static void Main1(string[] args)
+        {
+            int sum = 0;
+
+            #region for문
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("{0}회 반복...", i);
+
+            #endregion
+
+            #region 1부터 10까지 합
+            for (int i = 1; i <= 10; i++)
+                sum += i;
+
+            Console.WriteLine("1부터 10까지 합 :" + sum);
+            #endregion
+
+            #region 1부터 10까지 짝수합
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                    sum += i;
+            }
+            Console.WriteLine("1부터 10까지 짝수합 :" + sum);
+
+
+            #endregion
+
+            #region 중첩 for문
+            for (int a = 0; a <= 3; a++)
+            {
+                Console.WriteLine("a : " + a);
+                for (int b = 0; b <= 4; b++)
+                    Console.WriteLine("a : " + b);
+            }
+            #endregion
+
+            #region 구구단
+
+            for (int i = 2; i < 10; i++)
+            {
+                Console.WriteLine($"{0}단 시작", i);
+                for (int j = 1; j < 10; j++)
+                    Console.WriteLine($"{i} * {j} = {i*j}");
+            }
+
+            #endregion
+
+            #region 별 삼각형
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write("★");
+
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 10 - i; j > 0 ; j--)
+                    Console.Write("★");
+
+                Console.WriteLine();
+            }
+            #endregion
+        }
     }
 }
--- Ch03/4_While.cs
+++ Ch03/4_While.cs
@@ -6,7 +6,68 @@ using System.Threading.Tasks;
 
 namespace Ch03
 {
+    // p167
     internal class _4_While
     {
+        static void Main(string[] args)
+        {
+            #region 1부터 10까지 합
+            int sum = 0;
+            int k = 1;
+
+            while (k <= 10)
+            {
+                sum += k;
+                k++;
+            }
+            Console.WriteLine("1부터 10까지 합 {0}", sum);
+            #endregion
+
+            #region do ~ while
+            int total = 0;
+            int i = 1;
+            // 최초 한 번은 무조건 반복 실행하는 반복문
+            do
+            {
+                if (i % 2 == 0)
+                    total += i;
+
+                i++;
+            }
+            while (i <= 10);
+
+            Console.WriteLine("1 ~ 10까지 짝수의 합 " + total);
+            #endregion
+
+            #region break
+            int num = 1;
+
+            while (true)
+            {
+                if(num % 5 == 0 && num % 7 == 0)
+                break;
+
+                num++;
+            }
+            Console.WriteLine("5와 7의 최소 공배수 : {0}", num);
+            #endregion
+
+            #region continue
+            int tot = 0;
+            int j = 0;
+
+            while (j <= 10)
+            {
+                j++;
+
+                if (j % 2 == 1)
+                continue;
+
+                tot += j;
+            }
+            Console.WriteLine("1 ~ 10 까지 짝수의 합 : {0}", tot);
+            #endregion
+
+        }
     }
 }
