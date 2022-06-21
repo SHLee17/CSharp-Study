28b7853427deebd7efa9a26ac85481b033d28ac3
azurite17@gmail.com <azurite17@gmail.com>
2022-06-21 17:20:38 +0900

    2022/06/21

--- Baekjoon/9단계/Main9_04.cs
+++ Baekjoon/9단계/Main9_04.cs
@@ -4,67 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Baekjoon._9단계
 {
     internal class Main9_04
     {
-
-        static char[,] map = new char[3000, 3000];
+        static int a = 3000;
+        static char[,] map = new char[a, a];
+        
 
         static void Main()
         {
-            int n = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    map[i, j] = ' ';
-                }
-            }
-
-            FillStar(n, 0, 0);
             StringBuilder sb = new StringBuilder();
 
-            for (int i = 0; i < n; i++)
+            for (int y = 0; y < a; y++)
             {
-                for (int j = 0; j < n; j++)
+                for (int x = 0; x < a; x++)
                 {
-                    sb.Append(map[i, j]);
+                    map[y, x] = ' ';
+                }
+            }
+            int n = int.Parse(Console.ReadLine());
+            Star(n, 0, 0);
+
+            for (int y = 0; y < n; y++)
+            {
+                for (int x = 0; x < n; x++)
+                {
+                    sb.Append(map[y, x]);
                 }
                 sb.Append("\n");
             }
-
             Console.WriteLine(sb);
-
         }
 
-
-        //별을 채웁니다.
-        static void FillStar(int n, int y, int x)
+        static void Star(int n, int y, int x)
         {
-            if (n == 1)
+            if( n == 1)
             {
                 map[y, x] = '*';
-                Console.Write($"({n},{y},{x})");
                 return;
             }
-            else 
-            Console.Write($"(f{n},{y},{x})");
             int div = n / 3;
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
                     if (i == 1 && j == 1)
-                    {
-                        Console.Write($"(빈{n},{y},{x})");
                         continue;
-                    }
-                    FillStar(div, y + (div * i), x + (div * j));
+
+
+                    Star(div, y + (div * i), x + (div * j));
                 }
-                Console.WriteLine();
+
             }
+
             return;
         }
     }
